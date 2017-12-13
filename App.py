38bdd92a122d3c0730d2fffa6b19795a3f20c648
Kacper Bak <github@kacperbak.de>
2017-12-13 15:58:35 +0100

    First steps with Python

--- App.py
+++ App.py
@@ -1,9 +1,25 @@
 import sys
 
 
+def execute_triple_quoted_string():
+    triple_qutoed_string = """"Hello" world'!'"""
+    print(triple_qutoed_string)
+
+
+def use_input_and_output():
+    input_as_int = int(input("Please type in a number: "))
+    print(input_as_int)
+
+
 def main():
-    print("Hello python3")
-    print(sys.argv)
+    execute_triple_quoted_string()
+    use_input_and_output()
+
+    # arg0 = sys.argv[0]
+    # arg1 = sys.argv[1]
+    # print("0 argument: " + arg0)
+    # print("1st argument: " + arg1)
+ 
 
 
 main()
