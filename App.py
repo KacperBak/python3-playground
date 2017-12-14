06bcc6336638a8d66a971134f44bad1589602d00
Kacper Bak <github@kacperbak.de>
2017-12-14 15:24:23 +0100

    String operations added

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
 import sys
-
+import string
 
 def use_triple_quoted_string():
     triple_qutoed_string = """"Hello" world'!'"""
@@ -25,8 +25,39 @@ def use_passed_arguments(args):
         print(arg)
 
 
+def use_remove_all_whitespace():
+    # Print input_string
+    input_string = "          - Before -\tMiddle\t- End -        "
+    print("Raw input:" + input_string)
+
+    # Escape whitespaces on start and on the end
+    print("strip:" + input_string.strip())
+
+    # Escape whitespaces on start, end and in the middle
+    for character in string.whitespace:
+        input_string = input_string.replace(character, "")
+    print("replace:" + input_string)
+
+
+def use_repr_method():
+    # Create an object of type list
+    x = [1,2,3]
+
+    # Convert the object to a string
+    str0 = "This is my list: " + repr(x)
+
+    print(str0)
+
+
+def use_format_parameter():
+    positional_parameter_string = """'{0}' is the food of user '{1}'""".format("Steak", "Kacper")
+    named_parameter_string = """'{food}' is the food of user '{user}'""".format(food="Steak", user="Kacper")
+    print(positional_parameter_string)
+    print(named_parameter_string)
+
+
 def main():
-    use_passed_arguments(sys.argv)
+    use_format_parameter()
 
 
 main()
